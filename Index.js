55fd5260d6fda712bcd24232303e7655ab18cd24
Israel Berlanga <israel0688@hotmail.com>
2020-05-24 19:33:35 -0500

    strugling with calling functions

--- Index.js
+++ Index.js
@@ -1,5 +1,72 @@
+const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer.js');
 const Manager = require('./lib/Manager.js');
-const Inter = require('./lib/Intern')
+const Inter = require('./lib/Intern');
 const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template.js');
 
+const getEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name: ',
+            
+            validate: answer =>{
+                if (answer) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
+
+
+        },
+
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Enter id#: ',
+            validate: idInput =>{
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter id#!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Enter email: '
+        }
+    ])
+
+    .then(({name}) => {
+        this.employee = new Employee(name);
+    });
+
+}
+
+function managerQuestion() {
+    getEmployee()
+        .then(officeNumber())
+}
+
+function init() {
+    managerQuestion()
+        .then(generatePage)
+
+        .then(pageProfile => {
+            return writeFile(pageProfile);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+init();
---
+++ src/page-template.js
@@ -0,0 +1,54 @@
+module.exports = templateData => {
+    // destructure page data by section
+    const { projects, about, ...header } = templateData;
+  
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Profile Generator</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+      <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+      rel="stylesheet"
+    />
+    </head>
+  
+    <body>
+      <header>
+
+      
+        <div class="container">
+            <div class="row">
+                <div class="col-12 display-4 bg-dark text-white text-center rounded-lg">My Team</div>
+            </div>
+        </div>
+
+      </header>
+      <main class="container my-5">
+
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+   
+    </html>
+    `;
+  };
